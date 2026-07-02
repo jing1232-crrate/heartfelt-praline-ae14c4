9570d79de7656a37b67ed2e2b18f72fc065c3e80
jing1232-crrate <jingxian008@gmail.com>
2026-07-02 18:30:05 +0800

    2/7 update
    remove mouse grow and change hero image location

--- js/script.js
+++ js/script.js
@@ -107,36 +107,3 @@ const valueSwiper = new Swiper(".valueSwiper", {
     }
 
 });
-
-/*==========================
-Mouse Glow
-==========================*/
-
-const glow = document.querySelector(".mouse-glow");
-
-let mouseX = 0;
-let mouseY = 0;
-
-let currentX = 0;
-let currentY = 0;
-
-document.addEventListener("mousemove",(e)=>{
-
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-
-});
-
-function animate(){
-
-    currentX += (mouseX-currentX)*0.05;
-    currentY += (mouseY-currentY)*0.05;
-
-    glow.style.left = currentX+"px";
-    glow.style.top = currentY+"px";
-
-    requestAnimationFrame(animate);
-
-}
-
-animate();
