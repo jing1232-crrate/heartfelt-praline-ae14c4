ab0f5a79c3f60dcbcf159c0d4364713a47ebaa22
jing1232-crrate <jingxian008@gmail.com>
2026-07-13 17:30:56 +0800

    update

--- js/script.js
+++ js/script.js
@@ -107,3 +107,78 @@ const valueSwiper = new Swiper(".valueSwiper", {
     }
 
 });
+const modal = document.getElementById("policyModal");
+
+document.querySelector(".policy-link").addEventListener("click", function(e){
+    e.preventDefault();
+    modal.style.display = "flex";
+});
+
+document.querySelector(".close-policy").onclick = function(){
+    modal.style.display = "none";
+};
+
+window.onclick = function(e){
+    if(e.target === modal){
+        modal.style.display = "none";
+    }
+};
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", async function (e) {
+
+    e.preventDefault();
+
+    // Required
+    if (!form.checkValidity()) {
+        form.reportValidity();
+        return;
+    }
+
+    // Email
+    const email = form.email.value.trim();
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!form.checkValidity()) {
+
+    alert("Please complete the form correctly.");
+
+    form.reportValidity();
+
+    return;
+}
+
+    const data = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: email,
+        company: form.company.value,
+        position: form.position.value,
+        income: form.income.value,
+        concern: form.concern.value,
+        service: form.service.value,
+        message: form.message.value
+    };
+
+    try {
+
+        await fetch("https://script.google.com/macros/s/AKfycby9TXXpRul1aDoD_UgRULcQGuEEu0Gpjpy1m0s962xmODL_VonTMG0g3ESbkrRM5FV3/exec", {
+            method: "POST",
+            headers: {
+                "Content-Type": "text/plain;charset=utf-8"
+            },
+            body: JSON.stringify(data)
+        });
+
+        alert("Your enquiry has been submitted successfully!");
+        form.reset();
+
+    } catch (err) {
+
+        alert("Submission failed.");
+
+    }
+
+});
